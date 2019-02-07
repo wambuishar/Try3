afd012c61e4be187bffc6ea3fb49772452496ca1
darclive <wambuisha100@gmail.com>
2019-02-07 16:30:19 +0300

    added the profile view

---
+++ resources/views/profile.blade.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+ 
+
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/bootstrap.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/hover.css') }}">
+
+
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+</head>
+<style type="text/css">
+	
+	
+
+</style>
+
+<body>
+
+<div class="container-fluid" style="margin-top:50px">
+   <div class="container">
+   <div class="row">
+    <div class="col-sm-4" style="background-color:; height:300px;" >
+
+    <div class="button" style="background-color:grey ; border-color: blue;border-radius: 50%;height:150px;width: 140px;margin-left: auto; margin-right: auto;margin-top: 20px;"></div>
+    </div>
+    <div class="col-sm-8" style="background-color: ; height:300px;" >
+    	<div class="row" style="margin-top: 0px;">
+    		<div class="col-sm-6"><h2>User name</h2></div>
+    		<div class="col-sm-6"><button onclick="#ahref" style="border-radius:2px; border-color: grey;font-size:15px; margin-top: 25px;">Edit Profile</button></div>
+    	</div>
+    	  <div class="row" style="font-size: 20px;">
+            <div class="col-sm-4">
+    	
+    	 <span>posts</span>
+        </div>
+          <div class="col-sm-4">
+         <span>Followers</span>	
+        </div>
+        <div class="col-sm-4">
+          <span>Following</span>	
+        </div>
+     
+         </div> 
+        <div class="bio" style="font-size: 20px; margin-top: 10px;">bio <br>
+    	bio<br></div>
+        
+        </div>
+
+         </div>
+<div class="row">
+	<div class="col-sm-12" style="background-color:;height: 70px;"><div style="text-align: center;font-size: 20px;margin-top: 10px;">POSTS</div></div>
+</div>
+<div class="row" style="height: 500px;">
+	<div class="col-sm-4" >
+		<div class="card h-75"></div>
+		
+	</div>
+	<div class="col-sm-4">
+		<div class="card h-75"></div>
+	 </div>
+	<div class="col-sm-4">
+		<div class="card h-75"></div>
+	</div>
+ </div>
+ <div class="row" style="height: 600px;">
+	<div class="col-sm-4" >
+		<div class="card h-75"></div>
+		
+	</div>
+	<div class="col-sm-4">
+		<div class="card h-75"></div>
+	 </div>
+	<div class="col-sm-4">
+		<div class="card h-75"></div>
+	</div>
+ </div>
+</div>
+
+</div>
+  		
+
+  
+
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,7 @@ Route::get('/', function () {
 Route::get('/welcome', function () {
     return view('welcome');
 });
+	
+Route::get('/profile', function () {
+    return view('profile');
+});
